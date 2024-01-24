4b9e5b5af02c619c4afa45287429487e837a1333
Kevin Yi <officialyi@berkeley.edu>
2024-01-23 19:58:11 -0600

    Added index.js

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+$(".icon").hide().fadeIn(600).animate({opacity:1}, {duration: 300});
+console.log("hello world")
+  
+$(".menu-section").hide().fadeIn("600").animate({opacity: 1}, 300);
+ console.log() 
+
+$(".favorites").fadeIn("duration:4000").animate({opacity:1}, {duration:
+600});
+  $(".logistics").fadeIn("duration:4000").animate({opacity:1}, {duration: 
+600});
